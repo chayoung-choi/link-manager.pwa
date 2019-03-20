84a0cbc63ea89879da018c0de636a0ffb4fae9c1
chayoung_DK <chayoung.dev@gmail.com>
2019-03-20 12:51:46 +0900

    #27
    path test

--- scripts/app.js
+++ scripts/app.js
@@ -31,7 +31,7 @@
 
   var app = angular.module("myApp", ["ngRoute"]);
   app.config(function($routeProvider, $locationProvider) {
-      $locationProvider.html5Mode(true);
+      // $locationProvider.html5Mode(true);
 
       $routeProvider
       .when("/", {
