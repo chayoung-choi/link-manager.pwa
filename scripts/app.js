d81ab78dfc74083fd2e949d192d3fe7e58c0d956
chayoung.choi <chayoung.dev@gmail.com>
2019-03-30 10:21:17 +0900

    기능: 서버 데이터로 LinkCard View
    기능: 서버 데이터로 LinkCard View

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,14 @@
 
   var appStorage = {
     appPath: "/link-manager.pwa",
-    appVer: {verName: "0.0.5", verCode:"20190329.01"},
+    appVer: {verName: "0.0.7", verCode:"20190330.01"},
     id : "",
     hostList  : [],
     // isLoading: true,
-    // visibleCards: {},
+    visibleCards: {},
     // selectedCities: [],
     // spinner: document.querySelector('.loader'),
-    // cardTemplate: document.querySelector('.cardTemplate'),
+    // cardTemplate: document.querySelector('template').content,
     // container: document.querySelector('.main'),
     // addDialog: document.querySelector('.dialog-container'),
     daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -65,21 +65,29 @@ console.log("index appStorage #0", appStorage);
 
   // #0. init
   app.controller("initCtrl", function($scope, $location){
-console.log("[controller:initCtrl]");
+    console.log("[controller:initCtrl]");
+
     // $scope.pathname = $location.absUrl();
     // document.getElementsByTagName("base").href = $location.absUrl();
     var appVer = appStorage.appVer;
     $scope.appVer = appVer;
 
     // #0-1. [GET] 서버 Link List
-    $scope.updateLinkList = function() {
-console.log("[GET] 서버 Link List");
-      appStorage.updateLinkList();
+    $scope.getLinkList = function() {
+      console.log("[GET] 서버 Link List");
+      appStorage.getLinkList();
+    };
+
+    $scope.updateLinkCard = function() {
+      console.log("[GET] 서버 Link List");
+      // appStorage.getLinkList();
+       appStorage.updateLinkCard();
     };
   });
 
   app.controller("linksCtrl", function ($scope) {
-console.log("[controller:linksCtrl]");
+    console.log("[controller:linksCtrl]");
+
       $scope.names = [
         {name:'Jani',country:'Norway'},
         {name:'Hege',country:'Sweden'},
@@ -93,17 +101,19 @@ console.log("[controller:linksCtrl]");
   });
 
   app.controller('httpCtrl', function($scope, $http, $location) {
-console.log("signIn>init appStorage #1", appStorage);
-    $scope.signIn = function(){
+    console.log("[controller:httpCtrl]");
 
+    $scope.signIn = function(){
+      console.log("[function:signIn]");
+
+      // 로그인버튼 클릭 시 id input disabled
       $("#signIn-btn")[0].disabled = true;
       $("#signIn-spinner").removeClass("d-none");
       $("[name='username'")[0].disabled= true;
 
-      /* 파라메터로 보낼 임의의 데이터 객체 */
+      // 파라메터로 보낼 임의의 데이터 객체
       var sheet_name = "host";
       var id = CryptoJS.SHA256($scope.username).toString().toUpperCase();
-      console.log($scope.username, id);
 
       $http({
         method: 'GET',
@@ -114,14 +124,16 @@ console.log("signIn>init appStorage #1", appStorage);
 
         appStorage.id = id;
         appStorage.hostList = response.data.list;
-console.log("signIn>appStorage #2", appStorage);
+        console.log("[function:signIn>http-success>appStorage]", appStorage);
 
+        // success시 links view로 이동
         $location.path("links");
 
       }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
-        console.log(response.data);
+        console.log("[function:signIn>http-error>response.data]", response.data);
+
         $("#signIn-btn")[0].disabled = false;
         $("#signIn-spinner").addClass("d-none");
         $("[name='username'")[0].disabled= false;
@@ -140,26 +152,83 @@ console.log("signIn>appStorage #2", appStorage);
 
 
 //------------------------------------------------------------------------------
-  appStorage.updateLinkList = function() {
-console.log("appStorage.updateLinkList");
-    // var keys = Object.keys(app.visibleCards);
-    // keys.forEach(function(key) {
-    var id = appStorage.id;
-    appStorage.getLinkList(id);
-    // });
+/*
+  * appStorage *
+  // reloadLinkList()
+  updateLinkList(dataList)
+  updateLinkCard(data)
+  getLinkList() - 서버 통신
+*/
+// [fn:appStorage.updateLinkCard] - card 업데이트
+  appStorage.reloadLinkList = function(data) {
+
+  }
+
+// [fn:appStorage.updateLinkCard] - card 업데이트
+  appStorage.updateLinkCard = function(data) {
+    console.log("[appStorage.fn:updateLinkCard]");
+
+    var cardTemplate = document.querySelector('template').content;
+
+    if (!data){
+      data = initLinkData;
+    }
+
+    var seq      = data.seq;
+    var category = data.category;
+    var subSeq   = data.sub_seq;
+    var label    = data.label;
+    var pathname = data.pathname;
+    var search   = data.search;
+    var createdDate  = new Date(data.created);
+    var updatedDate  = new Date(data.updated);
+
+    console.log("[appStorage.fn:updateLinkCard>seq]", seq);
+    console.log("[appStorage.fn:updateLinkCard>createdDate]", createdDate);
+
+    // var card = appStorage.visibleCards[data.seq];
+    var card = cardTemplate.cloneNode(true);
+    card.querySelector('.card-header').textContent = data.category;
+    card.querySelector('.card-text').textContent = data.label;
+
+    var container = document.querySelector('.card-columns');
+    container.appendChild(card);
+    // if (!card) {
+    //   card.classList.remove('cardTemplate');
+    //   card.querySelector('.location').textContent = data.label;
+    //   card.removeAttribute('hidden');
+    //   app.container.appendChild(card);
+    //   app.visibleCards[data.key] = card;
+    // }
+
+  }
+
+  // [fn:appStorage.updateLinkList] - link data 업데이트
+  appStorage.updateLinkList = function(dataList) {
+    console.log("[appStorage.fn:updateLinkList>dataList]", dataList);
+
+    for (var i=0; i<dataList.length; i++){
+      console.log("[appStorage.fn:updateLinkList>dataList["+i+"]", dataList[i]);
+      var seq = dataList[i].seq;
+      appStorage.updateLinkCard(dataList[i]);
+    }
+
   };
 
 
 //------------------------------------------------------------------------------
-  appStorage.getLinkList = function(id) {
-    console.log("[appStorage.getLinkList]");
+  // [fn:appStorage.getLinkList] - 서버에서 link data 가져오기
+  appStorage.getLinkList = function() {
+    console.log("[appStorage.fn:getLinkList]");
+
+    var id = appStorage.id;
     var url = 'https://script.google.com/macros/s/AKfycbzblyyKhXtgiWvkQaWRMObrq1BrazFJ1Bae2DEH5GQqg3VwMVM/exec?sheet_name=links&id='+id;
 
     if ('caches' in window) {
       caches.match(url).then(function(response) {
         if (response) {
           response.json().then(function updateFromCache(json) {
-console.log("[appStorage.getLinkList] #1 json", json);
+            console.log("[appStorage.fn:getLinkList] #1 json", json);
 //             var results = json.query.results;
 // console.log("[appStorage.getLinkList] #2 results", results);
             // results.key = key;
@@ -176,22 +245,34 @@ console.log("[appStorage.getLinkList] #1 json", json);
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           var response = JSON.parse(request.response);
-          // var results = response.query.results;
-console.log("[appStorage.getLinkList] #3 response", response);
+          var results = response.list;
+console.log("[appStorage.fn:getLinkList] #3 results", results);
+          // console.log("[appStorage.fn:getLinkList]", response);
+          appStorage.updateLinkList(results);
           // results.key = key;
           // results.label = label;
           // results.created = response.query.created;
           // app.updateForecastCard(results);
         }
       } else {
-console.log("[appStorage.getLinkList] #4 err", response);
+console.log("[appStorage.fn:getLinkList] #4 err", response);
         // app.updateForecastCard(initialWeatherForecast);
       }
     };
     request.open('GET', url);
     request.send();
   };
-
+//------------------------------------------------------------------------------
+  var initLinkData = {
+    seq: 1,
+    category: "관리시스템",
+    sub_seq: 1,
+    label: "SMOSMGR20",
+    pathname: "SMOSMGR/",
+    search: "",
+    created: "2019-03-19T15:00:00.000Z",
+    updated: "2019-03-20T15:00:00.000Z"
+  }
 //------------------------------------------------------------------------------
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
